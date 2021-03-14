12178af58b5d053d1e1a98d4490adad6ba856baf
zhao-donglei <1303264850@qq.com>
2021-03-14 23:09:37 +0800

    rabbitMQ搭建
    简单队列

---
+++ rabbitMQ/src/main/java/com/alex/simple/recv/Recv.java
@@ -0,0 +1,38 @@
+package com.alex.simple.recv;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * 简单队列-消息消费者
+ */
+public class Recv {
+
+    //定义队列
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("47.116.128.174");
+        factory.setUsername("root");
+        factory.setVirtualHost("/demo");
+        factory.setPassword("alex");
+        factory.setPort(5672);
+        //连接工厂创建连接
+        Connection connection = factory.newConnection();
+        //创建信道
+        Channel channel = connection.createChannel();
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
